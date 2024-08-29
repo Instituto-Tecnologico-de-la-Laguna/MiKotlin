4191acd04f1199ade097f03fd86e898ad55ca7e4
jmot4 <jmota@gtech-services.com>
2024-08-28 23:44:44 -0600

    Agregando ; para realizar prueba

--- src/main.kt
+++ src/main.kt
@@ -2,6 +2,6 @@ fun main() {
     println("Hello, World") //Proyecto de Inicio 2
     println("Agregar nombre del alumno");
     println("Agregando linea de condigo");
-
+    println("Luis Mota")
 
 }
