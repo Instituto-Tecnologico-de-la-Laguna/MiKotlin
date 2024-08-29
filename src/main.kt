2386a7a227929aeab12340d2779780470921d2be
jmot4 <jmota@gtech-services.com>
2024-08-28 23:43:16 -0600

    Agregando ; para realizar prueba

--- src/main.kt
+++ src/main.kt
@@ -1,6 +1,7 @@
 fun main() {
     println("Hello, World") //Proyecto de Inicio 2
-    println("Agregar nombre del alumno")
-    println("Agregando linea de condigo")
+    println("Agregar nombre del alumno");
+    println("Agregando linea de condigo");
+
 
 }
