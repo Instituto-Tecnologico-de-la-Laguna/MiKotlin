2c3c365361888538ca4af1ed49c8ea469b205c73
LuisBarranco <nezzlu87@gmail.com>
2024-08-29 23:35:15 -0600

    se agrego la entrada de 4 valores, se sumaron los primeros dos, se multiplicaron los ultimos dos.

--- src/main.kt
+++ src/main.kt
@@ -1,3 +1,25 @@
 fun main() {
-    println("Hello, World") //LuisBarranco
+    //PRACTICA 1
+    //Luis Ernesto Barranco Nuñez
+
+    //ENTRADA DE DATOS
+    print("Ingrese primer valor:")
+    val valor1 = readln().toInt()
+
+    print("Ingrese segundo valor:")
+    val valor2 = readln().toInt()
+
+    print("Ingrese tercer valor:")
+    val valor3 = readln().toInt()
+
+    print("Ingrese cuarto valor:")
+    val valor4 = readln().toInt()
+
+    //OPERACION SUMA Y MOSTRAMOS RESULTADO
+    val suma = valor1 + valor2
+    println("La suma de $valor1 y $valor2 es $suma")
+
+    //OPERACION MULTIPLICACION Y MOSTRAMOS RESULTADO
+    val producto = valor3 * valor4
+    println("El producto de $valor3 y $valor4 es $producto")
 }
