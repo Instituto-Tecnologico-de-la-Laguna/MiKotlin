8699834c37340925ce746f6fffbc977deff8c448
ferth23 <torresherrerafernanda1@gmail.com>
2024-08-29 20:28:06 -0600

    Práctica 1 - 21130859 María Fernanda Torres Herrera

--- src/main.kt
+++ src/main.kt
@@ -1,3 +1,21 @@
 fun main() {
-    println("Hello, World")
+    println ( "Ingrese cuatro números:" )
+
+    print ( "1: " )
+    val n1 = readln().toInt()
+
+    print ( "2: " )
+    val n2 = readln().toInt()
+
+    print ( "3: " )
+    val n3 = readln().toInt()
+
+    print ( "4: " )
+    val n4 = readln().toInt()
+
+    val suma = n1 + n2
+    val prod = n3 * n4
+
+    println ( "La suma de los números $n1 y $n2 es: $suma" )
+    println ( "El producto de los números $n3 y $n4 es: $prod" )
 }
