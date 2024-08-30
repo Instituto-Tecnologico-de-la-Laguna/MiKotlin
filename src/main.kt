dd8ae749d04a24b03b4639843725460457d0c340
David Cárdenas González <davidcardenasgonz@gmail.com>
2024-08-30 13:30:33 -0600

    feat: add initial functionality

--- src/main.kt
+++ src/main.kt
@@ -1,3 +1,15 @@
 fun main() {
-    println("Hello, World")
+    print("Ingrese primer valor:")
+    val valor1 = readLine()!!.toInt()
+    print("Ingrese segundo valor:")
+    val valor2 = readLine()!!.toInt()
+    val suma = valor1 + valor2
+    println("La suma de $valor1 y $valor2 es $suma")
+
+    print("Ingrese tercer valor:")
+    val valor3 = readLine()!!.toInt()
+    print("Ingrese cuarto valor:")
+    val valor4 = readLine()!!.toInt()
+    val producto = valor3 * valor4
+    println("El producto de $valor3 y $valor4 es $producto")
 }
