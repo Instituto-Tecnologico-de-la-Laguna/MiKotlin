1a190f729383c6510677b048c088903576a25e78
EdsonAleman910 <edsonalemancu@gmail.com>
2024-08-30 00:09:07 -0600

    Se insertó el codigo para la realización de la practica 1: Ingresar 4 números y obtener e resultado de la suma de los primeros 2 y de la multiplicación de los ultimos 2.

--- src/main.kt
+++ src/main.kt
@@ -1,3 +1,17 @@
 fun main() {
-    println("Hello, World")
+    //println("Hello, World")//Edson Alemán Cueto
+    println("Ingresa el primer número:")
+    val x1 = readln().toInt()
+    println("Ingresa el segundo número")
+    val x2 = readln().toInt()
+    println("Ingresa el tercer número")
+    val x3 = readln().toInt()
+    println("Ingresa el cuarto número")
+    val x4 = readln().toInt()
+
+    val suma = x1 + x2
+    val multi = x3 * x4
+
+    println("La suma de $x1 + $x2 es: $suma")
+    println("El producto de $x3 * $x4 es: $multi")
 }
