59be0017cfcbbebbe3e30298323fd13231c6d937
Miriam Sánchez <miriamaliciasanchezcervantes@gmail.com>
2024-08-29 21:14:24 -0600

    Practica 1 21130882 Miriam Sanchez

--- src/main.kt
+++ src/main.kt
@@ -1,3 +1,17 @@
 fun main() {
-    println("Hello, World")//Miriam Alicia Sanchez Cervantes
+
+    println("Ingresa el primer numero:")
+    val num1 = readln().toInt()
+    println("Ingrese el segundo numero:")
+    val num2 = readln().toInt()
+    println("Ingrese el tercer numero:")
+    val num3 = readln().toInt()
+    println("Ingrese el cuarto numero:")
+    val num4 = readln().toInt()
+
+    val suma = num1 + num2
+    val producto = num3*num4
+
+    println("La suma de los primeros numeros es $suma")
+    println("El producto de los ultimos numeros es $producto")
 }
