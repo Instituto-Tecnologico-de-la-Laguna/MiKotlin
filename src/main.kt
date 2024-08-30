93f586c8929d16ed70ede603a1b379d21d42187e
JOSE EDUARDO GIJON <alu.21130883@correo.itlalaguna.edu.mx>
2024-08-30 10:55:33 -0600

    Se cambio el parametro de la linea 2 en la funcion println por Hola mundo, Bienvenido a mi primer programa kotlin!!!
    se cumplio con los requisitos de la practica, un pequeño programa que lea primero 2 numeros, imprima la suma de estos y luego vuele a leer otros 2 numeros reasignando nuevos valores a las variables anteriormete creadas para mostrar el producto de estos, luego con un pequeño ciclo pregunta al usuario si desea salir de la aplicacion
    --Jose Eduardo Gijon Mora

--- src/main.kt
+++ src/main.kt
@@ -1,3 +1,31 @@
 fun main() {
-    println("Hola mundo!!!") //MI HUMILDE CONTRIBUCION GIJON MORA JOSE EDUARDO
+    println("Hola mundo, Bienvenido a mi primer programa kotlin!!!") //MI HUMILDE CONTRIBUCION GIJON MORA JOSE EDUARDO
+
+    do{
+        var num1: Int
+        var num2: Int
+
+        println("Ingrese un numero: ")
+        num1 = readln().toInt()
+
+        println("Ingrese otro numero: ")
+        num2 = readln().toInt();
+
+        println("La suma de $num1 y $num2 es = ${num1+num2}")
+
+        println("Pulsa enter para continuar...")
+
+        readLine()
+
+        println("Ingrese un numero: ")
+        num1 = readln().toInt()
+
+        println("Ingrese otro numero: ")
+        num2 = readln().toInt()
+
+        println("La multiplicacion entre $num1 y $num2 es = ${num1*num2}")
+
+        println("Salir de la aplicacion? (S/N)")
+        var opcion = readln()
+    }while(opcion != "S")
 }
