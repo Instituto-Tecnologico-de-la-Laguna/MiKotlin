8669f0ecf47d6d88213afe4129d23bcc33a0e09b
SebasROCS <sebasrdz88@gmail.com>
2024-08-29 23:32:40 -0600

    PRACTICA #1 Suma de dos valores y multiplicacion de otros dos valores.

--- src/main.kt
+++ src/main.kt
@@ -1,3 +1,16 @@
 fun main() {
-    println("Hello, World")// SEBASTIAN R
+    //SEBASTIAN RODRIGUEZ CERVANTES
+    print("Ingrese primer valor:")
+    val valor1 = readln().toInt()
+    print("Ingrese segundo valor:")
+    val valor2 = readln().toInt()
+    val suma = valor1 + valor2
+    println("La suma de $valor1 y $valor2 es $suma")
+
+    print("Ingrese tercer valor:")
+    val valor3 = readln().toInt()
+    print("Ingrese cuarto valor:")
+    val valor4 = readln().toInt()
+    val producto = valor3 * valor4
+    println("El producto de $valor3 y $valor4 es $producto")
 }
