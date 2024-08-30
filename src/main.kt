73b4e3fdb8a6c3da79283db40b1ee81063dd5ac3
Vectorr22 <vmtg208@hotmail.com>
2024-08-29 22:04:25 -0600

    Practica 1. Lectura y manipulación de numeros para mostrar suma y producto usando operadores aritmeticos.

--- src/main.kt
+++ src/main.kt
@@ -1,3 +1,25 @@
 fun main() {
-    println("Hello, World") //Victorr
+    //Desarrollado por Victor Tijerina
+    println("Bienvenido a la practica 1: ")
+    println("Inserte el primer valor: ")
+    val number1 = readln().toInt()
+
+    println("Inserte el segundo valor: ")
+    val number2 = readln().toInt()
+
+    println("Inserte el tecer valor: ")
+    val number3 = readln().toInt()
+
+    println("Inserte el cuarto valor: ")
+    val number4 = readln().toInt()
+
+    println("La suma del numero 1 y el numero 2 es: ${
+        number1+number2
+    }"
+    )
+
+    println("El producto del numero 3 y el numero 4 es: ${
+        number3*number4
+    }"
+    )
 }
