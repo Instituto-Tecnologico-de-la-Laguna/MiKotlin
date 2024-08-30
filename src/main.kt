85f30eb20938241b168d1e7698f4df494d02ef0d
Vanessa <laylavanessa11@gmail.com>
2024-08-29 22:06:06 -0600

    Práctica 1 - Programa que toma cuatro valores enteros, muestra la suma de los primeros dos, y la multiplicación de los últimos dos.

--- src/main.kt
+++ src/main.kt
@@ -1,3 +1,20 @@
 fun main() {
-    println("Hello, World") // Layla Vanessa Glz Mtz
+    // Layla Vanessa Glz Mtz
+        print("Ingrese el primer valor:")
+        val num1 = readln().toInt()
+
+        print("Ingrese el segundo valor:")
+        val num2 = readln().toInt()
+
+        print("Ingrese tercer valor:")
+        val num3 = readln().toInt()
+
+        print("Ingrese segundo valor:")
+        val num4 = readln().toInt()
+
+        val suma = num1 + num2
+        println("La suma del primer valor ($num1) y del segundo ($num2) es $suma")
+
+        val producto = num3 * num4
+        println("El producto del tercer valor ($num3) y el cuarto ($num4) es $producto")
 }
