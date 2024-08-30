2dceb6e6bd0ed0c6fc8d58995bdce45aab8eb027
RodMac08 <rodrigoprime@outlook.es>
2024-08-30 13:15:47 -0600

    Pracitca 1, se realizo la suma de los 2 primeros numeros ingresados y el producto de los 2 ultimos

--- src/main.kt
+++ src/main.kt
@@ -1,3 +1,12 @@
 fun main() {
-    println("Hello, World")//Rodrigo
+    val n1 = readln().toInt()
+    val n2 = readln().toInt()
+    val n3 = readln().toInt()
+    val n4 = readln().toInt()
+
+    val suma = n1 + n2
+    val prod = n3 * n4
+
+    println("La suma es $suma")
+    println("El producto es $prod")
 }
