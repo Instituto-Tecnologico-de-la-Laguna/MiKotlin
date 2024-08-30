4cf7f6c7268d2407f42f1c8fdb5a613756348571
Humberto-MS <humberto.ms123@gmail.com>
2024-08-29 18:58:23 -0600

    Practica 1 - Humberto Medina Santos - 21130862

--- src/main.kt
+++ src/main.kt
@@ -1,3 +1,19 @@
 fun main() {
-    println("Hello, World") // Humberto Medina :P
+    print("Ingrese el primer valor: ")
+    val v1 = readln().toInt()
+
+    print("Ingrese el segundo valor: ")
+    val v2 = readln().toInt()
+
+    print("Ingrese el tercer valor: ")
+    val v3 = readln().toInt()
+
+    print("Ingrese el cuarto valor: ")
+    val v4 = readln().toInt()
+
+    val suma = v1 + v2
+    println ( "\nLa suma de $v1 y $v2 es $suma" )
+
+    val producto = v3 * v4
+    println ( "El producto de $v3 y $v4 es $producto" )
 }
