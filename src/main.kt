a4838af916d4f473eaca1763fa715fd0d34b336e
scarl <lunasecena@gmail.com>
2024-08-29 22:36:47 -0600

    Se realizo la practica 1: ingresar 4 numeros y realizar la suma de los primeros dos y la multiplicacion del tercero y el cuarto.

--- src/main.kt
+++ src/main.kt
@@ -1,3 +1,19 @@
+
 fun main() {
-    println("Hello, World")
+   // println("Hello, World")//Scarlett
+    println("Ingresa el primer numero:")
+    val n1 = readln().toInt()
+    println("Ingresa el segundo numero")
+    val n2 = readln().toInt()
+    println("Ingresa el tercer numero")
+    val n3 = readln().toInt()
+    println("Ingresa el cuarto numero")
+    val n4 = readln().toInt()
+
+    val suma = n1 + n2
+    val producto = n3 * n4
+
+    println("La suma de $n1 + $n2 es: $suma")
+    println("El producto de $n3 * $n4 es: $producto")
+
 }
