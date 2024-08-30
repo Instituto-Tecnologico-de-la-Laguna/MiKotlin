c38fee818b5164a96c510c62c4bafb263a0e9aa9
paucasvil <paulina161104@gmail.com>
2024-08-29 22:06:07 -0600

    Practica 1 Programa que recibe 4 valores enteros, devuelve la suma de los primeros 2 y el producto de los otros 2 valores.

--- src/main.kt
+++ src/main.kt
@@ -1,3 +1,23 @@
+//Paulina Jaqueline Castañeda Villalobos
+//21130850
+//Práctica 1
 fun main() {
-    println("Hello, World") //Paulina Jaqueline Castañeda
+
+    print("Ingrese primer valor:")
+    val val1 = readln().toInt()
+
+    print("Ingrese segundo valor:")
+    val val2 = readln().toInt()
+
+    print("Ingrese tercer valor:")
+    val val3 = readln().toInt()
+
+    print("Ingrese cuarto valor:")
+    val val4 = readln().toInt()
+
+    val sum = val1 + val2
+    println("La suma de $val1 y $val2 es $sum")
+
+    val prod = val3 * val4
+    println("El producto de $val3 y $val4 es $prod")
 }
