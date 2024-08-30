b028cbfc3e94054b4597104aeaffcb0d16974c37
ValBadillo <alu.21130896@correo.itlalaguna.edu.mx>
2024-08-29 22:19:09 -0600

    programa para calcular suma y producto de cuatro números

--- src/main.kt
+++ src/main.kt
@@ -1,3 +1,20 @@
 fun main() {
-    println("Hello, World")//Valeria Badillo Gutierrez
+    println("Ingresar numero uno")
+    val num1=readLine()!!.toInt()
+
+    println("Ingresar numero dos")
+    val num2=readLine()!!.toInt()
+
+    println("Ingresar numero tres")
+    val num3=readLine()!!.toInt()
+
+    println("Ingresar numero cuatro")
+    val num4=readLine()!!.toInt()
+
+    val suma = num1+num2
+    val producto =num3*num4
+
+    println("La suma de los dos numeros es:$suma")
+    println("El producto de los siguientes numeros es:$producto")
+
 }
