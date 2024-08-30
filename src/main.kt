d4139f9982881674deaaea5d5be52a4a8266ff5e
nefi2307 <alu.21130937@correo.itlalaguna.edu.mx>
2024-08-30 13:23:16 -0600

    Práctica 1 Se crearon cuatro variables las cuales se piden al usuario, las dos primeras se suman y las dos ultimas se multiplican, imprimiendo los valores dados y el resultado de sus operaciones obtenido en pantalla.

--- src/main.kt
+++ src/main.kt
@@ -1,3 +1,21 @@
 fun main() {
-    println("Hello, World") // Isaac Burciaga Chacón 21130937
+    // println("Hello, World") // Isaac Burciaga Chacón 21130937
+
+    // Suma
+    print("Ingrese el primer valor: ")
+    val valor1 = readln().toInt()
+    print("Ingrese el segundo valor: ")
+    val valor2 = readln().toInt()
+    val suma = valor1 + valor2
+    // Producto
+    print("Ingrese el tercer valor: ")
+    val valor3 = readln().toInt()
+    print("Ingrese el cuarto valor: ")
+    val valor4 = readln().toInt()
+    val producto = valor3 * valor4
+
+    println("La suma de $valor1 y $valor2 es igual a $suma" )
+    print("El producto de $valor3 y $valor4 es igual a $producto")
+
+
 }
