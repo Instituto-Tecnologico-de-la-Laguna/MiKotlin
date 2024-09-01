a39a5db5fff359ea60250a22e7cc5228729d96f1
Miguel Garza <miguelgrza.12@gmail.com>
2024-09-01 14:43:09 -0600

    Added the sum of first two numbers

--- src/main.kt
+++ src/main.kt
@@ -1,3 +1,10 @@
 fun main() {
-    println("Hello, World")
+    print("Ingrese el primer valor:")
+    val valor1 = readln().toInt()
+    print("Ingrese el segundo valor:")
+    val valor2 = readln().toInt()
+    print("Ingrese el tercer valor:")
+
+    val suma = valor1 + valor2
+    println("La suma de $valor1 y $valor2 es $suma")
 }
